04c2c2aca0275a09d32bd42eb58d7a524dad2ea2
Al3xiel <gabriel280104casas@gmail.com>
2025-07-12 12:18:22 -0500

    feat(home): implement home screen with branding and navigation

---
+++ lib/views/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Center(
+              child: Text("AppBook",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue
+                )
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(child: Image.asset('images/brand-logo.png')),
+            SizedBox(height: 20),
+            Center(
+                child:
+                OutlinedButton(
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/search');
+                    },
+                    child:
+                    Text("Find Book",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue
+                      )
+                    )
+                ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child:
+              OutlinedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/search');
+                  },
+                  child:
+                  Text("Favorite Book",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
+                      )
+                  )
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}

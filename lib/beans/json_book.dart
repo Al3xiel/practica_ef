52b5aa13ada494ea79b2cf34b274e1e18e186b18
Al3xiel <gabriel280104casas@gmail.com>
2025-07-12 18:30:13 -0500

    feat(model): update JsonBook class to include url and publishedAt fields

--- lib/beans/json_book.dart
+++ lib/beans/json_book.dart
@@ -1,33 +1,37 @@
 class JsonBook {
   String? title;
   String? author;
+  String? url;
   String? urlToImage;
   String? content;
-  String? description;
+  String? publishedAt;
 
   JsonBook({
     this.title,
     this.author,
+    this.url,
     this.urlToImage,
     this.content,
-    this.description,
+    this.publishedAt,
   });
 
   JsonBook.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     author = json['author'];
+    url = json['url'];
     urlToImage = json['urlToImage'];
     content = json['content'];
-    description = json['description'];
+    publishedAt = json['publishedAt'];
   }
 
   Map<String, dynamic> toJson() {
     return {
       'title': title,
       'author': author,
+      'url': url,
       'urlToImage': urlToImage,
       'content': content,
-      'description': description,
+      'publishedAt': publishedAt,
     };
   }
 }
